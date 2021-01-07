933f847739bde1742e5d0edceb32b9c3786e17d7
reymon359 <ramonmorcillocascales@gmail.com>
2021-01-07 21:07:28 +0100

    feat: retrieve posts from users using include

--- script.ts
+++ script.ts
@@ -5,8 +5,11 @@ const prisma = new PrismaClient()
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  // use `console.dir` to print nested objects
+  console.dir(allUsers, { depth: null })
 }
 
 main()
