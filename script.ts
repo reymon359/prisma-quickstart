eee3f4997eedf39c2bf3fcc283e6c4d134a5df13
reymon359 <ramonmorcillocascales@gmail.com>
2021-01-07 21:11:07 +0100

    feat: insert new post

--- script.ts
+++ script.ts
@@ -5,6 +5,17 @@ const prisma = new PrismaClient()
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
+  // Insert new post
+  const post = await prisma.post.create({
+    data: {
+      title: "Prisma makes databases easy",
+      author: {
+        connect: { email: "sarah@prisma.io" },
+      },
+    },
+  })
+  console.log(post)
+
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
   })
